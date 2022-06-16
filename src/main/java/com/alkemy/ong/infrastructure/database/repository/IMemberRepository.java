3d4b388bfefb46da7469230b98b9ccbe1fd75e2f
evincere <elsemper@gmail.com>
2022-06-16 17:09:49 -0300

    modelo y repositorio base de Members creado

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/IMemberRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/IMemberRepository.java
@@ -1,5 +1,10 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
-public interface IMemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.alkemy.ong.infrastructure.database.entity.Member;
+
+@Repository
+public interface IMemberRepository extends JpaRepository<Member, Long> {
 
 }
