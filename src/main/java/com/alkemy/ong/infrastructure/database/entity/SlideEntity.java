9c6f763f47a1ee2b842c983bc5b9c8979ba984be
Juan Carlos Ojeda <69003838+ojedajuanc@users.noreply.github.com>
2022-06-16 18:42:21 -0300

    OT231-28: create model and repository Slides (#3)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/SlideEntity.java
@@ -0,0 +1,34 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "SLIDES")
+public class SlideEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "SLIDE_ID")
+  private Long id;
+
+  @Column(name = "IMAGE_URL", nullable = false)
+  private String imageUrl;
+
+  @Column(name = "TEXT")
+  private String text;
+
+  @Column(name = "POSITION", nullable = false)
+  private Integer order;
+
+}
