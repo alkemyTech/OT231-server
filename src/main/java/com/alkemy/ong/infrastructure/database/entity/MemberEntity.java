d78a5874a0828a6dd56f683ca2df784e4a3df2bc
evincere <elsemper@gmail.com>
2022-06-16 08:36:47 -0300

    modelo y repositorio base de Members

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/MemberEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/MemberEntity.java
@@ -1,41 +1,57 @@
 package com.alkemy.ong.infrastructure.database.entity;
 
-import java.util.Calendar;
-
+import java.security.Timestamp;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.lang.Nullable;
 
-@Entity
-@Table(name = "members")
 @Getter
 @Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "members")
 @SQLDelete(sql = "UPDATE members SET deleted = true WHERE id = ?")
-@Where(clause = "deleted = false")
+@Where(clause = "softDeleted = false")
 public class MemberEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "MEMBER_ID")
+  private Long fooId;
+  
+  @Column(name = "NAME")
   @NotNull
   private String name;
+  
   @Nullable
   private String facebookUrl;
+  
   @Nullable
   private String instagramUrl;
+  
   @Nullable
   private String linkedinUrl;
+  
   @NotNull
   private String image;
+  
   @Nullable
   private String description;
-  @SuppressWarnings("unused")
-  private boolean deleted = Boolean.FALSE;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Calendar registDate;
+  
+  @Column(name = "CREATE_TIMESTAMP")
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETED")
+  private Boolean softDeleted;
 }
