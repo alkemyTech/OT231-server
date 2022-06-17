574e090a42e4315cf414d72191ff68e030def5b3
evincere <elsemper@gmail.com>
2022-06-17 13:30:35 -0300

    ticket corregido

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/TestimonialEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/TestimonialEntity.java
@@ -8,43 +8,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-import org.springframework.lang.Nullable;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "TESTIMONIALS")
-@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id = ?")
-@Where(clause = "softDeleted = false")
-public class Testimonial {
-  
+public class TestimonialEntity {
+
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "TESTIMONIAL_ID")
-  private Long testimonialId;
-  
-  @Column(name = "NAME")
-  @NotNull
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
   private String name;
-  
+
   @Column(name = "IMAGE")
-  @Nullable
   private String image;
-  
+
   @Column(name = "CONTENT")
-  @Nullable
   private String content;
-  
+
   @Column(name = "CREATE_TIMESTAMP")
   @CreationTimestamp
   private Timestamp createTimestamp;
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ItestimonialRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ItestimonialRepository.java
@@ -1,10 +1,10 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
-import com.alkemy.ong.infrastructure.database.entity.Testimonial;
+import com.alkemy.ong.infrastructure.database.entity.TestimonialEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItestimonialRepository extends JpaRepository<Testimonial, Long> {
+public interface ItestimonialRepository extends JpaRepository<TestimonialEntity, Long> {
 
 }
