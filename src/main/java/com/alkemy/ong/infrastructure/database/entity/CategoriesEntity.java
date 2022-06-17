09bbfe58ce2c22828aca3c5bd45e5e92a62e5c4a
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-17 16:52:08 -0300

    OT231-18 : Add Categories Entity Imports

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
@@ -1,13 +1,15 @@
 package com.alkemy.ong.infrastructure.database.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.*;
 import java.sql.Timestamp;
-
 
 @Entity
 @NoArgsConstructor
