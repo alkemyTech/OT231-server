23456e11cf0ea7e11e4b8b9d9c9b356d5e7e72eb
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-17 18:34:27 -0300

    OT231-15 : Add Organization Entity

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/OrganizationEntity.java
@@ -12,57 +12,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
-<<<<<<< HEAD
-@Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Data
 @Table(name = "ORGANIZATIONS")
-public class OrganizationEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    Column(name = "NAME", nullable = false)
-    private String name;
-
-    Column(name = "IMAGE", nullable = false)
-    private String image;
-
-    Column(name = "IMAGE_URL", nullable = false)
-    private String imageUrl;
-
-    Column(name = "ADDRESS")
-    private String addres;
-
-    Column(name = "IMAGE_URL", nullable = false)
-    private String imageUrl;
-
-    Column(name = "PHONE")
-    private String phone;
-
-    Column(name = "EMAIL", nullable = false)
-    private String email;
-
-    Column(name = "WELCOME_TEXT", nullable = false)
-    private String welcomeText;
-
-    Column(name = "ABOUT_US_TEXT")
-    private String imageUrl;
-
-    @Column(name = "CREATE_TIMESTAMP")
-    @CreationTimestamp
-    private Timestamp createTimestamp;
-
-    @Column(name = "SOFT_DELETE")
-    private Boolean softDelete;
-=======
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Table(name = "ORGANITAZIONS")
 public class OrganizationEntity {
 
   @Id
@@ -96,5 +50,6 @@ public class OrganizationEntity {
 
   @Column(name = "SOFT_DELETE")
   private Boolean softDelete;
->>>>>>> OT231-18
+
+
 }
