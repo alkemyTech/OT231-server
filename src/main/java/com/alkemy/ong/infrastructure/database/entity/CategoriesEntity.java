8256bd5804b6f8f24cb768ca2a1a087b091798dc
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-17 16:22:30 -0300

    OT231-18 : Add Categories Entity Args

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
@@ -1,10 +1,17 @@
 package com.alkemy.ong.infrastructure.database.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Table(name = "CATEGORIES")
 public class CategoriesEntity {
@@ -13,8 +20,21 @@ public class CategoriesEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    
+    @Column(name = "NAME")
+    private String name;
 
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "IMAGE")
+    private String image;
+
+    @Column(name = "CREATE_TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp createTimestamp;
+
+    @Column(name = "SOFT_DELETE")
+    private Boolean softDelete;
 
 
 }
