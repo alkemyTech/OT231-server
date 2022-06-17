01992574fad7a131771ee52e5ab452f7bc2dd781
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-17 15:25:18 -0300

    OT231-18 : Rename Categories Entity Class

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/Categories.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.ong.infrastructure.database.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Categories {
-}
---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "CATEGORIES")
+public class CategoriesEntity {
+
+    
+}
