e45c037f5c2995304444cfa4e451d493175409c9
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-17 15:28:07 -0300

    OT231-18 : Add Categories an Id

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoriesEntity.java
@@ -2,13 +2,19 @@ package com.alkemy.ong.infrastructure.database.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "CATEGORIES")
 public class CategoriesEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     
+
+
+
 }
