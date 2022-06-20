56bd4975cf8f3102608da10000a7884deef4e4db
evincere <elsemper@gmail.com>
2022-06-20 19:47:55 -0300

    OT231-20: add Testimonial entity and repository

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/TestimonialEntity.java
@@ -0,0 +1,42 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TESTIMONIALS")
+public class TestimonialEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "TESTIMONIAL_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "IMAGE")
+  private String image;
+
+  @Column(name = "CONTENT")
+  private String content;
+
+  @Column(name = "CREATE_TIMESTAMP")
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETE")
+  private Boolean softDelete;
+}
