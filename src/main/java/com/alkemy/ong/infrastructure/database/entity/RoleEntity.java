bbabddd45a1d12d09abed3ffcec82dd628082b5f
juuancarrizo <31081219+juuancarrizo@users.noreply.github.com>
2022-06-20 05:16:50 -0300

    OT231-16: create Model and repository role (#8)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/RoleEntity.java
@@ -0,0 +1,36 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ROLES")
+public class RoleEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "ROLE_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  @Column(name = "CREATE_TIMESTAMP")
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+}
---
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/IRoleRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.infrastructure.database.repository;
+
+import com.alkemy.ong.infrastructure.database.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<RoleEntity, Long> {
+}
