fabae9c47b28edd623579082305219617578d5bb
ucielbustamante <106886104+ucielbustamante@users.noreply.github.com>
2022-06-21 14:05:33 -0300

    OT231-19: create news entity and repository (#17)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/NewsEntity.java
@@ -0,0 +1,51 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "NEWS")
+public class NewsEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "NEWS_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "CONTENT", nullable = false)
+  private String content;
+
+  @Column(name = "IMAGE", nullable = false)
+  private String image;
+
+  @Column(name = "CREATE_TIMESTAMP")
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETE")
+  private Boolean softDelete;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "CATEGORY_ID")
+  private CategoryEntity category;
+
+}
