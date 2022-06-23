33f0eb638647f814da8b13884aa8753c4ae0357a
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-23 09:32:24 -0300

    OT231-25 : Add Authentication Endpoint file

---
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/AuthenticationEndpoint.java
@@ -0,0 +1,44 @@
+package com.alkemy.ong.infrastructure.config.spring.security;
+
+import com.alkemy.ong.infrastructure.config.spring.security.common.JwtUtils;
+import com.alkemy.ong.infrastructure.config.spring.security.models.AuthenticationResponse;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationEndpoint {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtTokenUtil;
+
+  @RequestMapping(value = "/auth/login ", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody UserEntity userEntity) throws Exception{
+
+      try {
+          authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userEntity.getEmail(), userEntity.getPassword()));
+      }
+      catch (BadCredentialsException e){
+          throw new Exception("Incorrect username or password", e);
+      }
+      final UserDetails userDetails = userDetailsService.loadUserByUsername(userEntity.getEmail());
+      final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+      return ResponseEntity.ok(new AuthenticationResponse(jwt));
+  }
+}
