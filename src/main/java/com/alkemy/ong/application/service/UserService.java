3a58d7d6e6c118cde18392052be358fa3a82ab5f
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-23 20:52:26 -0300

    OT231-24: add UserService and use case in application package

---
+++ src/main/java/com/alkemy/ong/application/service/UserService.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.application.service;
+
+import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
+import com.alkemy.ong.domain.User;
+
+public class UserService implements ICreateUserUseCase {
+
+  @Override
+  public User addUser(User newUser) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
