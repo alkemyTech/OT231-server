3586bae63fe4e617752a72d19e565ead7f2cbe7d
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-23 14:04:42 -0300

    OT231-25 : Add Security

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().permitAll();
     http.csrf().disable()
             .authorizeRequests().antMatchers("/auth/login").permitAll()
             .anyRequest().authenticated();
