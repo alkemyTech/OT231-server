c858a890bf229792d4e1a068fdc43c60c25b2d7f
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-23 09:37:18 -0300

    OT231-25 : Edit Security Config adding Authentication path

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -56,7 +56,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll();
+    http.csrf().disable()
+            .authorizeRequests().antMatchers("/auth/login").permitAll()
+            .anyRequest().authenticated();
   }
 
 }
