4ee657a47c99a32e2db3139e6fe2d26630fafd5f
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-24 11:36:57 -0300

    OT231-24: minor changes in UserResponse

--- src/main/java/com/alkemy/ong/infrastructure/rest/response/UserResponse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/UserResponse.java
@@ -1,18 +1,20 @@
 package com.alkemy.ong.infrastructure.rest.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
+@Builder
 public class UserResponse {
 
   private String firstName;
-
   private String lastName;
-
   private String email;
 
 }
