17290388586981748f683b3062da3c4b3d4c237d
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-24 13:27:11 -0300

    OT231-24: modify configure method in SecurityConfig
    Modified antMatcher to permit requests from /auth/*

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().cors().and().sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/auth/login").permitAll().anyRequest().authenticated().and()
+        .antMatchers(HttpMethod.POST, "/auth/*").permitAll().anyRequest().authenticated().and()
         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
         .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
         .authenticationEntryPoint(authenticationEntryPoint());
