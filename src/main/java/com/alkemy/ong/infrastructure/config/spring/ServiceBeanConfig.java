eabd3bd28fd9e174e493682d87f3b349679c97e4
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-24 13:26:32 -0300

    OT231-24: add createUserService Bean

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/ServiceBeanConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/ServiceBeanConfig.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.infrastructure.config.spring;
 
 import com.alkemy.ong.application.service.AuthenticationService;
+import com.alkemy.ong.application.service.UserService;
+import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
 import com.alkemy.ong.application.service.usecase.ILoginUseCase;
 import com.alkemy.ong.infrastructure.database.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,11 @@ public class ServiceBeanConfig {
   public ILoginUseCase authenticationService(AuthenticationManager authenticationManager,
       UserRepository userRepository) {
     return new AuthenticationService(authenticationManager, userRepository);
+  }
+
+  @Bean
+  public ICreateUserUseCase createUserService(UserRepository userRepository) {
+    return new UserService(userRepository);
   }
 
 }
