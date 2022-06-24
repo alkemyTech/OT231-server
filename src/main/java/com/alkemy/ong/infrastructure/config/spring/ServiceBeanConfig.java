c288ecd8ce29a2f2870df5d0abecac8f97867c52
Magali Kain <magali.kain@thoughtworks.com>
2022-06-24 14:00:42 +0200

    OT231-25: create Spring bean configuration

---
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/ServiceBeanConfig.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.infrastructure.config.spring;
+
+import com.alkemy.ong.application.service.AuthenticationService;
+import com.alkemy.ong.application.service.usecase.ILoginUseCase;
+import com.alkemy.ong.infrastructure.database.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@Configuration
+public class ServiceBeanConfig {
+
+  @Bean
+  public ILoginUseCase authenticationService(AuthenticationManager authenticationManager,
+      UserRepository userRepository) {
+    return new AuthenticationService(authenticationManager, userRepository);
+  }
+
+}
