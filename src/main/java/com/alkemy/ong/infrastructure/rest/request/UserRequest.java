2f36502c87b055bb09664465373a2e8977d3cd6a
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-23 22:41:45 -0300

    OT231-24: add constraints validation to UserRequest

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/UserRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/UserRequest.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +12,16 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserRequest {
 
+  @Pattern(regexp = "^\\p{L}+[\\p{L}\\s]*$")
   private String firstName;
 
+  @Pattern(regexp = "^\\p{L}+[\\p{L}\\s]*$")
   private String lastName;
 
+  @Email
   private String email;
 
+  @Size(min = 6, max = 8)
   private String password;
 
 }
