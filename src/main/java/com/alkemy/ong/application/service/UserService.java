a68b80fdcdd2eb52e776425f57a83e26776f6c09
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-24 11:41:44 -0300

    OT231-24: modify UserService & IUserRepository
    Implemented addUser method in UserSerivice
    Declared addUser method in IUserRepository

--- src/main/java/com/alkemy/ong/application/service/UserService.java
+++ src/main/java/com/alkemy/ong/application/service/UserService.java
@@ -1,14 +1,21 @@
 package com.alkemy.ong.application.service;
 
+import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
 import com.alkemy.ong.domain.User;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class UserService implements ICreateUserUseCase {
+
+  private final IUserRepository userRepository;
 
   @Override
   public User addUser(User newUser) {
-    // TODO Auto-generated method stub
-    return null;
+    if (userRepository.findByEmail(newUser.getEmail()) != null) {
+      // TODO "This email address is already being used"
+    }
+    return userRepository.addUser(newUser);
   }
 
 }
