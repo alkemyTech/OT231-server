cca3c7078ce3a8b1304460c859f1afa514c202f3
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-24 16:59:15 -0300

    OT231-24: add default Role when a User is created
    Implemented logic to add a default USER rol to every new User that is created.

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/UserRepository.java
@@ -2,17 +2,22 @@ package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.database.entity.RoleEntity;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
 import com.alkemy.ong.infrastructure.database.mapper.UserEntityMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
 public class UserRepository implements IUserRepository {
 
   private final IUserSpringRepository userSpringRepository;
+  private final IRoleSpringRepository roleSpringRepository;
   private final UserEntityMapper userEntityMapper;
+
+  private String defaultRole = "USER";
 
   @Override
   public User findByEmail(String email) {
@@ -20,9 +25,20 @@ public class UserRepository implements IUserRepository {
   }
 
   @Override
+  @Transactional
   public User addUser(User newUser) {
     UserEntity user = userEntityMapper.toEntity(newUser);
+    user.setRole(getDefaultRole());
     return userEntityMapper.toDomain(userSpringRepository.save(user));
+  }
+
+  private RoleEntity getDefaultRole() {
+    if (roleSpringRepository.findByName(defaultRole) != null) {
+      return roleSpringRepository.findByName(defaultRole);
+    }
+    RoleEntity role = new RoleEntity();
+    role.setName(defaultRole);
+    return roleSpringRepository.save(role);
   }
 
 }
