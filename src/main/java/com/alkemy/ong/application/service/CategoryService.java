a73db2f1b324dc96f7a8aeef96a1a754d9929291
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-25 19:18:09 -0300

    OT231-44 : Add Category Service

---
+++ src/main/java/com/alkemy/ong/application/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.application.service;
+
+import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
+
+public class CategoryService implements IDeleteCategoryUseCase {
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
