a2572e99069f0c3bfa36fe48ffd76189855bfff3
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-25 21:04:32 -0300

    OT231-44 : Edit Category Service

--- src/main/java/com/alkemy/ong/application/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/application/service/CategoryService.java
@@ -1,11 +1,19 @@
 package com.alkemy.ong.application.service;
 
+import com.alkemy.ong.application.exception.InvalidCredentialsException;
+import com.alkemy.ong.application.repository.ICategoryRepository;
 import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class CategoryService implements IDeleteCategoryUseCase {
 
+    ICategoryRepository categoryRepository;
     @Override
     public void delete(Long id) {
+        if (!categoryRepository.exitsById(id) || categoryRepository.isDeleted(id))
+        throw new InvalidCredentialsException("test");
 
+    categoryRepository.delete(id);
     }
 }
