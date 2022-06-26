848b23bce1ca1ca700f02daf74e9ec268d614aff
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-25 21:08:47 -0300

    OT231-44 : Edit ICategorySpringRepository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategorySpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategorySpringRepository.java
@@ -2,8 +2,15 @@ package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICategorySpringRepository extends JpaRepository<CategoryEntity, Long> {
+
+    @Query("SELECT u FROM CategoryEntity u WHERE u.softDelete = true")
+    Boolean isDeleted(Long id);
+
+    @Query("UPDATE u WHERE CategoryEntity u SET u.sofDelete = true WHERE u.id = ?1")
+    void sofDeleteById(Long id);
 }
