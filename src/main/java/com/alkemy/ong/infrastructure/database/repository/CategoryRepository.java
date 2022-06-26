a1ec97c0686ff919ca462bd027f87f73ea87672a
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-25 21:07:53 -0300

    OT231-44 : Edit CategoryRepository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
@@ -1,16 +1,30 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
-import com.alkemy.ong.infrastructure.database.mapper.CategoryEntityMapper;
+import com.alkemy.ong.application.repository.ICategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor
 @Component
-public class CategoryRepository {
+public class CategoryRepository implements ICategoryRepository {
 
-    private final ICategorySpringRepository categorySpringRepository;
-    private final CategoryEntityMapper categoryRepository;
+  ICategorySpringRepository iCategorySpringRepository;
 
+  @Override
+    public boolean exitsById(Long id) {
+        return iCategorySpringRepository.existsById(id);
+  }
 
+  @Override
+    public boolean isDeleted(Long id) {
+        return iCategorySpringRepository.isDeleted(id);
+  }
 
+  @Override
+  @Transactional
+  public void delete(Long id){
+        iCategorySpringRepository.sofDeleteById(id);
+    }
 }
