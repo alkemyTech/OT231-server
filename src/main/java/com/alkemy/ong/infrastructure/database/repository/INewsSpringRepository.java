e2ff56754bf7a3b611e47a281638509cd02b6323
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-27 13:15:03 -0300

    OT231-48: add query methods to INewsSpringRepository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/INewsSpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/INewsSpringRepository.java
@@ -1,10 +1,20 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.NewsEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface INewsSpringRepository extends JpaRepository<NewsEntity, Long> {
+
+  @Query("SELECT u FROM NewsEntity u WHERE u.softDelete = true")
+  Optional<NewsEntity> isDeleted(Long id);
+
+  @Modifying
+  @Query("UPDATE NewsEntity u SET u.softDelete = true WHERE u.id = ?1")
+  void softDeleteById(Long id);
 
 }
