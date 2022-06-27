f66a1a194d8b5e1110a9d3d95664442e280cef76
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-27 13:10:27 -0300

    OT231-48: add deleteNewsService Bean

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/ServiceBeanConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/ServiceBeanConfig.java
@@ -1,6 +1,9 @@
 package com.alkemy.ong.infrastructure.config.spring;
 
+import com.alkemy.ong.application.repository.INewsRepository;
 import com.alkemy.ong.application.service.AuthenticationService;
+import com.alkemy.ong.application.service.NewsService;
+import com.alkemy.ong.application.service.usecase.IDeleteNewsUseCase;
 import com.alkemy.ong.application.service.usecase.ILoginUseCase;
 import com.alkemy.ong.infrastructure.database.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,11 @@ public class ServiceBeanConfig {
   public ILoginUseCase authenticationService(AuthenticationManager authenticationManager,
       UserRepository userRepository) {
     return new AuthenticationService(authenticationManager, userRepository);
+  }
+
+  @Bean
+  public IDeleteNewsUseCase deleteNewsService(INewsRepository newsRepository) {
+    return new NewsService(newsRepository);
   }
 
 }
