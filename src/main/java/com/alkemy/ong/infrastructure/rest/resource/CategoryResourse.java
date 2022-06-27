d7cd1bbf32dbc265cd26f7af4b0c895ee80bb01d
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-27 16:24:51 -0300

    OT231-44 : Add Category Endpoint

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResourse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResourse.java
@@ -1,2 +1,22 @@
-package com.alkemy.ong.infrastructure.rest.resource;public class CategoryResourse {
+package com.alkemy.ong.infrastructure.rest.resource;
+
+import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryResourse {
+
+    @Autowired
+    private IDeleteCategoryUseCase deleteCategoryUseCase;
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteCategoryUseCase.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
 }
