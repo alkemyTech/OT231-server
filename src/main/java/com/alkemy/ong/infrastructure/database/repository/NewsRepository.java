30529ccb31fa2a4af14a76679c0f0bb4c7571f7a
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-27 13:15:37 -0300

    OT231-48: add NewsRepository

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/NewsRepository.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.infrastructure.database.repository;
+
+import com.alkemy.ong.application.repository.INewsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Component
+public class NewsRepository implements INewsRepository {
+
+  @Autowired
+  private final INewsSpringRepository newsSpringRepository;
+
+  @Override
+  public boolean existsById(Long id) {
+    return newsSpringRepository.existsById(id);
+  }
+
+  @Override
+  public boolean isDeleted(Long id) {
+    if (newsSpringRepository.isDeleted(id) == null) {
+      return false;
+    }
+    return newsSpringRepository.isDeleted(id).isPresent();
+  }
+
+  @Override
+  @Transactional
+  public void delete(Long id) {
+    newsSpringRepository.softDeleteById(id);
+  }
+
+}
