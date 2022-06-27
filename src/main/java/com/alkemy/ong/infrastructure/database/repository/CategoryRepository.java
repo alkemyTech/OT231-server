cf9df079ceff724821c8b369534b980719a5ef7b
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-27 16:23:38 -0300

    OT231-44 : Add Category Database Repository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
@@ -1,4 +1,34 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
-public class CategoryRepository {
+import com.alkemy.ong.application.repository.ICategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Component
+public class CategoryRepository implements ICategoryRepository {
+
+    @Autowired
+    private final ICategorySpringRepository categorySpringRepository;
+
+    @Override
+    public boolean existsById(Long id) {
+        return categorySpringRepository.existsById(id);
+    }
+
+    @Override
+    public boolean isDeleted(Long id) {
+        if (categorySpringRepository.isDeleted(id) == null) {
+            return false;
+        }
+        return categorySpringRepository.isDeleted(id).isPresent();
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        categorySpringRepository.softDeleteById(id);
+    }
 }
