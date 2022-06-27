fbe62468902d038eba26376423546f137e6e2f48
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-27 16:24:13 -0300

    OT231-44 : Add Category Database Interface method

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategorySpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategorySpringRepository.java
@@ -1,9 +1,20 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ICategorySpringRepository extends JpaRepository<CategoryEntity, Long> {
+
+    @Query("SELECT c FROM CategoryEntity c WHERE c.softDelete = true")
+    Optional<CategoryEntity> isDeleted(Long id);
+
+    @Modifying
+    @Query("UPDATE CategoryEntity c SET c.softDelete = true WHERE c.id = ?1")
+    void softDeleteById(Long id);
 }
