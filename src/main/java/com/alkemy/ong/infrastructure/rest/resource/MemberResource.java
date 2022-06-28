a3bbe806c0dbaac58ca057e8204fd05fcd4dbbaf
jackyos <jacky04_20@hotmail.com>
2022-06-28 00:29:10 -0300

    OT231-70: add MemberResource class

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/MemberResource.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.infrastructure.rest.resource;
+
+import com.alkemy.ong.application.service.usecase.IDeleteMemberUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("members")
+public class MemberResource {
+
+  @Autowired
+  private IDeleteMemberUseCase deleteMemberUseCase;
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    deleteMemberUseCase.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+
+
+
+}
