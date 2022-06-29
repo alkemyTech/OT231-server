cef961014346b03ef233e7b7e980335499e559bc
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-29 18:26:49 -0300

    OT231-64 : Add Social Media Links Build

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/OrganizationMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/OrganizationMapper.java
@@ -1,13 +1,16 @@
 package com.alkemy.ong.infrastructure.rest.mapper;
 
 import com.alkemy.ong.domain.Organization;
+import com.alkemy.ong.domain.OrganizationSocialMedia;
 import com.alkemy.ong.infrastructure.rest.response.OrganizationPublicDataResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Component
 public class OrganizationMapper {
 
-  public OrganizationPublicDataResponse toResponse(Organization organization) {
+  public OrganizationPublicDataResponse toResponse(Organization organization, OrganizationSocialMedia organizationSocialMedia) {
     if (organization == null) {
       return null;
     }
@@ -16,6 +19,9 @@ public class OrganizationMapper {
         .image(organization.getImage())
         .address(organization.getAddress())
         .phone(organization.getPhone())
+        .socialMedia(Collections.singletonList(organizationSocialMedia.getFacebookUrl()))
+        .socialMedia(Collections.singletonList(organizationSocialMedia.getInstagramUrl()))
+        .socialMedia(Collections.singletonList(organizationSocialMedia.getLinkedIndUrl()))
         .build();
   }
 
