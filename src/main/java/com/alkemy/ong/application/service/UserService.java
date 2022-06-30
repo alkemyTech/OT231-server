5710a12e07df043ef7df52308023ea05447b3916
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-30 20:50:16 -0300

    OT231-36: add IGetUserUseCase & implementation on UserService

--- src/main/java/com/alkemy/ong/application/service/UserService.java
+++ src/main/java/com/alkemy/ong/application/service/UserService.java
@@ -5,14 +5,18 @@ import com.alkemy.ong.application.exception.UserAlreadyExistsException;
 import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
+import com.alkemy.ong.application.service.usecase.IGetUserUseCase;
 import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.config.spring.security.common.JwtUtils;
 import com.alkemy.ong.infrastructure.config.spring.security.common.Role;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class UserService implements ICreateUserUseCase, IDeleteUserUseCase {
+public class UserService implements ICreateUserUseCase, IDeleteUserUseCase, IGetUserUseCase {
 
   private final IUserRepository userRepository;
+
+  private final JwtUtils jwtUtils;
 
   @Override
   public User add(User newUser) {
@@ -31,4 +35,19 @@ public class UserService implements ICreateUserUseCase, IDeleteUserUseCase {
     }
     userRepository.delete(id);
   }
+  
+  @Override
+  public User getDetails(String token) {
+    String email = jwtUtils.extractUsername(token);
+    return getUserBy(email);
+  }
+
+  private User getUserBy(String email) {
+    User user = userRepository.findByEmail(email);
+    if (user == null) {
+      throw new RecordNotFoundException("User not found.");
+    }
+    return user;
+  }
+  
 }
