c1c43008a7b154146f4453602a0e28dadc83e49a
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-30 10:07:54 -0300

    OT231-64 : Add Social Media domain

---
+++ src/main/java/com/alkemy/ong/domain/SocialMedia.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class SocialMedia {
+
+    private String facebookUrl;
+    private String instagramUrl;
+    private String linkedIndUrl;
+}
