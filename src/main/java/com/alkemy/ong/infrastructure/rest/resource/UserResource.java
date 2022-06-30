1932dad058477566ddb596f6a1b800256053ae16
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-30 20:48:43 -0300

    OT231-36: add endpoint to get user details

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
@@ -2,18 +2,24 @@ package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
+import com.alkemy.ong.application.service.usecase.IGetUserUseCase;
 import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.rest.mapper.AuthenticationMapper;
 import com.alkemy.ong.infrastructure.rest.mapper.UserRegisterMapper;
 import com.alkemy.ong.infrastructure.rest.request.UserRegisterRequest;
+import com.alkemy.ong.infrastructure.rest.response.AuthenticationResponse;
 import com.alkemy.ong.infrastructure.rest.response.UserRegisterResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +33,12 @@ public class UserResource {
 
   @Autowired
   private IDeleteUserUseCase deleteUserUseCase;
+  
+  @Autowired
+  private IGetUserUseCase getUserUseCase;
+  
+  @Autowired
+  private AuthenticationMapper authenticationMapper;
 
   @PostMapping(value = "/auth/register",
       produces = {"application/json"},
@@ -36,6 +48,14 @@ public class UserResource {
     User user = userRegisterMapper.toDomain(registerRequest);
     UserRegisterResponse response = userRegisterMapper.toResponse(createUserUseCase.add(user));
     return new ResponseEntity<UserRegisterResponse>(response, HttpStatus.CREATED);
+  }
+  
+  @GetMapping(value = "/auth/me", 
+      produces = {"application/json"})
+  public ResponseEntity<AuthenticationResponse> getDetails(@RequestHeader HttpHeaders headers) {
+    String jwt = headers.getFirst("Authorization");
+    AuthenticationResponse response = authenticationMapper.toResponse(getUserUseCase.getDetails(jwt));
+    return ResponseEntity.ok(response);
   }
 
   @DeleteMapping(value = "/users/{id}")
