f6d9d39d9f072e9a95b2e364125652d2a6eba429
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-06-30 15:11:52 -0300

    OT231-64 : Add Social Media Builder

--- src/main/java/com/alkemy/ong/infrastructure/database/mapper/OrganizationEntityMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/mapper/OrganizationEntityMapper.java
@@ -1,25 +1,32 @@
 package com.alkemy.ong.infrastructure.database.mapper;
 
 import com.alkemy.ong.domain.Organization;
+import com.alkemy.ong.domain.SocialMedia;
 import com.alkemy.ong.infrastructure.database.entity.OrganizationEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrganizationEntityMapper {
 
+
+
   public Organization toDomain(OrganizationEntity organizationEntity) {
     if (organizationEntity == null) {
       return null;
     }
+    SocialMedia socialMedia = SocialMedia.builder()
+        .facebookUrl(organizationEntity.getFacebookUrl())
+        .instagramUrl(organizationEntity.getInstagramUrl())
+        .linkedIndUrl(organizationEntity.getLinkedInUrl())
+        .build();
     return Organization.builder()
         .name(organizationEntity.getName())
         .image(organizationEntity.getImage())
         .address(organizationEntity.getAddress())
         .phone(organizationEntity.getPhone())
-        .facebookUrl(organizationEntity.getFacebookUrl())
-        .instagramUrl(organizationEntity.getInstagramUrl())
-        .linkedIndUrl(organizationEntity.getLinkedInUrl())
+        .socialMedia(socialMedia)
         .build();
+
   }
 
 }
