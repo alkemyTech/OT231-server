08bda65fb5be9050567d2eed37b142f69bd98fda
jackyos <jacky04_20@hotmail.com>
2022-07-01 01:08:42 -0300

    OT231-55: add Slide domain

---
+++ src/main/java/com/alkemy/ong/domain/Slide.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class Slide {
+
+  private String imageUrl;
+  private Integer order;
+
+}
