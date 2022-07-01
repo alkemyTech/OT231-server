35fba661fc79ffec783813fb09c0f575d446327a
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-06-30 22:17:17 -0300

    OT231-36: add integration test

---
+++ src/test/java/com/alkemy/ong/bigtest/user/GetUserInfoIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.bigtest.user;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.alkemy.ong.bigtest.BigTest;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+public class GetUserInfoIntegrationTest extends BigTest {
+  
+  @Test
+  public void shouldReturn403WhenMissingAuthToken() throws Exception {
+    mockMvc.perform(get("/auth/me")
+            .header(HttpHeaders.AUTHORIZATION, "INVALID_TOKEN"))
+        .andExpect(status().isForbidden());
+  }
+  
+  @Test
+  public void shouldReturnUserInfoWhenRequestHasValidToken() throws Exception {
+    mockMvc.perform(get("/auth/me")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
+        .andExpect(jsonPath("$.email", equalTo("freedy@krueger.com")))
+        .andExpect(jsonPath("$.firstName", equalTo("Freddy")))
+        .andExpect(jsonPath("$.lastName", equalTo("Krueger")))
+        .andExpect(jsonPath("$.photo", equalTo(null)))
+        .andExpect(jsonPath("$.token", notNullValue()))
+        .andExpect(status().isOk());
+  }
+}
