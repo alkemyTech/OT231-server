281f993f1a79f718fa7cda07413f175bd1558274
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-02 19:27:39 -0300

    OT231-42 : Rename Category Resource

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +20,9 @@ public class CategoryResourse {
     deleteCategoryUseCase.delete(id);
     return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
   }
+
+  @PostMapping(value = "/categories",
+          produces = {"application/json"},
+          consumes = {"application/json"})
+  public ResponseEntity<CategoryCreateResponse> create(
 }
