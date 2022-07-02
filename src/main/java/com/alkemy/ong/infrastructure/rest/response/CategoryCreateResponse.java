916c9eff05a56350760acc6e39e629a467d24ec3
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-02 18:12:39 -0300

    "OT231-42 : Add Category Response"

--- src/main/java/com/alkemy/ong/infrastructure/rest/response/CategoryCreateResponse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/CategoryCreateResponse.java
@@ -1,2 +1,20 @@
-package com.alkemy.ong.infrastructure.rest.response;public class CategoryCreateResponse {
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CategoryCreateResponse {
+
+    private String name;
+    private String description;
+    private String image;
+
 }
