c897b8fe86bab8fa8929cbdc723e324f4a969e4a
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-02 18:13:14 -0300

    "OT231-42 : Add Category Repository"

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
@@ -1,6 +1,10 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.ICategoryRepository;
+import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import com.alkemy.ong.infrastructure.database.mapper.CategoryEntityMapper;
 import com.alkemy.ong.infrastructure.database.repository.spring.ICategorySpringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,9 +16,24 @@ public class CategoryRepository implements ICategoryRepository {
 
   private final ICategorySpringRepository categorySpringRepository;
 
+  private CategoryEntityMapper categoryEntityMapper;
+
+  @Override
+  public Category findByName(String name) {
+    return categoryEntityMapper.toDomain(categorySpringRepository.findByName(name));
+  }
+
   @Override
   public boolean existsById(Long id) {
     return categorySpringRepository.existsById(id);
+
+  }
+
+  @Override
+  public Category add(Category newCategory) {
+    CategoryEntity categoryEntity = categoryEntityMapper.toEntity(newCategory);
+    categoryEntity.setSoftDelete(false);
+    return categoryEntityMapper.toDomain(categorySpringRepository.save(categoryEntity));
   }
 
   @Override
