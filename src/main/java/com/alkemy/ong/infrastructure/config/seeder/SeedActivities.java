1cdcd7daeea7a2899d0bea8cff84d29329822990
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-04 23:03:24 -0300

    Style that horrible thing right now or you will seegit add .

--- src/main/java/com/alkemy/ong/infrastructure/config/seeder/SeedActivities.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/seeder/SeedActivities.java
@@ -2,14 +2,13 @@ package com.alkemy.ong.infrastructure.config.seeder;
 
 import com.alkemy.ong.infrastructure.database.entity.ActivityEntity;
 import com.alkemy.ong.infrastructure.database.repository.spring.IActivitySpringRepository;
+import java.util.List;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.util.List;
 
 @Log
 @Configuration
@@ -20,9 +19,7 @@ public class SeedActivities {
   //If I not failing the bullet,
   // these constants we're created for in some case to use this images en other instances
   private static final String IMAGE_I = "juanito.png";
-
   private static final String IMAGE_II = "juanAlCuadrado.jpg";
-
   private static final String IMAGE_III = "magaliArribaEspaña.jpg";
 
   @Autowired
@@ -36,16 +33,17 @@ public class SeedActivities {
     };
   }
 
-  private void createStandarActivity(){
-if(activitySpringRepository.count() == 0){
-  activitySpringRepository.saveAll(List.of(
-          buildActivity("Fix the tree's", "More tree's" , IMAGE_I),
-          buildActivity("Create mor donation houses", "More donation and more houses" , IMAGE_II),
-          buildActivity("Integrate public food donation mail", "Food's" , IMAGE_III)));
-  log.info("Initial Activity Created");
-}
+  private void createStandarActivity() {
+    if (activitySpringRepository.count() == 0) {
+      activitySpringRepository.saveAll(List.of(
+          buildActivity("Fix the tree's", "More tree's", IMAGE_I),
+          buildActivity("Create mor donation houses", "More donation and more houses", IMAGE_II),
+          buildActivity("Integrate public food donation mail", "Food's", IMAGE_III)));
+      log.info("Initial Activity Created");
+    }
   }
-  private ActivityEntity buildActivity(String name, String content, String image){
+
+  private ActivityEntity buildActivity(String name, String content, String image) {
     return ActivityEntity.builder()
             .name(name)
             .content(content)
