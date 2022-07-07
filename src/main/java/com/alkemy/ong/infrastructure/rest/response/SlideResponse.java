074926626f2442ce1911f437bd73a57c15901337
Magali Kain <magali.kain@thoughtworks.com>
2022-07-07 22:56:47 +0200

    Add @JsonInclude(JsonInclude.Include.NON_NULL) to SlideResponse

--- src/main/java/com/alkemy/ong/infrastructure/rest/response/SlideResponse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/SlideResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.infrastructure.rest.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Builder
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SlideResponse {
 
   private String imageUrl;
