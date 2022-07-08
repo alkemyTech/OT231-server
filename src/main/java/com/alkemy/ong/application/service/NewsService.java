0d480c4503834eb1f01398d721161170e622b98e
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-08 09:52:16 -0300

    OT231-45 : Add Get One Methotd to NewsService and exitsNew methotd to fast coding

--- src/main/java/com/alkemy/ong/application/service/NewsService.java
+++ src/main/java/com/alkemy/ong/application/service/NewsService.java
@@ -4,11 +4,12 @@ import com.alkemy.ong.application.exception.RecordNotFoundException;
 import com.alkemy.ong.application.repository.INewsRepository;
 import com.alkemy.ong.application.service.usecase.ICreateNewsUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteNewsUseCase;
+import com.alkemy.ong.application.service.usecase.IGetOneNewUseCase;
 import com.alkemy.ong.domain.News;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class NewsService implements IDeleteNewsUseCase, ICreateNewsUseCase {
+public class NewsService implements IDeleteNewsUseCase, ICreateNewsUseCase, IGetOneNewUseCase {
 
   private final INewsRepository newsRepository;
 
@@ -26,4 +27,18 @@ public class NewsService implements IDeleteNewsUseCase, ICreateNewsUseCase {
     return newsRepository.add(news);
   }
 
+
+  @Override
+  public News getOne(Long id) {
+    if (!existNew(id)) {
+      throw new RecordNotFoundException("News not found.");
+    }
+  }
+
+  public Boolean existNew(Long id){
+    if (!newsRepository.existsById(id) || newsRepository.isDeleted(id)) {
+      return false;
+    }
+    return true;
+  }
 }
