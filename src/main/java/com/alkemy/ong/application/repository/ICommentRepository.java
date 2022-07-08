44993c5dafcda9ef84f092aff359a0c0ef4aafba
jackyos <jacky04_20@hotmail.com>
2022-07-08 19:50:14 -0300

    OT231-78: Add Delete Comment Repository interface

--- src/main/java/com/alkemy/ong/application/repository/ICommentRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/ICommentRepository.java
@@ -2,8 +2,13 @@ package com.alkemy.ong.application.repository;
 
 import com.alkemy.ong.domain.Comment;
 
+import java.util.Optional;
+
 public interface ICommentRepository {
 
   Comment add(Comment  comment);
 
+  Optional<Comment> findById(Long id);
+
+  void save(Comment comment);
 }
