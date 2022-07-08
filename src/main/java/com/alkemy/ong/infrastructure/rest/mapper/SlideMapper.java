cd11e2ae9b46b1f668a63773af580f66b8e8b847
Magali Kain <magali.kain@thoughtworks.com>
2022-07-08 10:55:52 +0200

    Refactor SlideMapper to avoid duplicate code

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/SlideMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/SlideMapper.java
@@ -3,6 +3,8 @@ package com.alkemy.ong.infrastructure.rest.mapper;
 import com.alkemy.ong.domain.Slide;
 import com.alkemy.ong.infrastructure.rest.response.ListSlideResponse;
 import com.alkemy.ong.infrastructure.rest.response.SlideResponse;
+import com.alkemy.ong.infrastructure.rest.response.field.SlideResponseField;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,23 +13,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class SlideMapper {
 
+  private static final SlideResponseField[] DEFAULT_RESPONSE_MAPPING = {
+      SlideResponseField.IMAGE_URL, SlideResponseField.ORDER};
+
   public ListSlideResponse toResponse(List<Slide> slides) {
+    return toResponse(slides, DEFAULT_RESPONSE_MAPPING);
+  }
+
+  public ListSlideResponse toResponse(List<Slide> slides, SlideResponseField... fields) {
     if (slides == null || slides.isEmpty()) {
       return new ListSlideResponse(Collections.emptyList());
     }
 
     List<SlideResponse> slideResponses = new ArrayList<>(slides.size());
     for (Slide slide : slides) {
-      slideResponses.add(toResponse(slide));
+      slideResponses.add(toResponse(slide, fields));
     }
     return new ListSlideResponse(slideResponses);
   }
 
   public SlideResponse toResponse(Slide slide) {
-    return SlideResponse.builder()
-        .imageUrl(slide.getImageUrl())
-        .order(slide.getOrder())
-        .build();
+    return toResponse(slide, DEFAULT_RESPONSE_MAPPING);
+  }
+
+  public SlideResponse toResponse(Slide slide, SlideResponseField... fields) {
+    SlideResponse slideResponse = new SlideResponse();
+    for (SlideResponseField field : fields) {
+      switch (field) {
+        case IMAGE_URL:
+          slideResponse.setImageUrl(slide.getImageUrl());
+          break;
+        case TEXT:
+          slideResponse.setText(slide.getText());
+          break;
+        case ORDER:
+          slideResponse.setOrder(slide.getOrder());
+          break;
+        default:
+          throw new UnsupportedOperationException(
+              MessageFormat.format("Field {0} is unsupported.", field));
+      }
+    }
+    return slideResponse;
   }
 
 }
---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/field/SlideResponseField.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.infrastructure.rest.response.field;
+
+public enum SlideResponseField {
+
+  IMAGE_URL,
+  TEXT,
+  ORDER;
+
+}
