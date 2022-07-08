07fedd9544b46c50108cd78c898da177d21cf4f5
jackyos <jacky04_20@hotmail.com>
2022-07-08 20:03:42 -0300

    OT231-78: Update CommentResource class

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.ICreateCommentUseCase;
+import com.alkemy.ong.application.service.usecase.IDeleteCommentUseCase;
 import com.alkemy.ong.domain.Comment;
 import com.alkemy.ong.infrastructure.rest.mapper.CommentMapper;
 import com.alkemy.ong.infrastructure.rest.request.CommentRequest;
@@ -9,9 +10,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CommentResource {
@@ -22,6 +27,9 @@ public class CommentResource {
   @Autowired
   private ICreateCommentUseCase createCommentUseCase;
 
+  @Autowired
+  private IDeleteCommentUseCase deleteCommentUseCase;
+
 
   @PostMapping(value = "/comments",
       produces = {"application/json"},
@@ -31,6 +39,13 @@ public class CommentResource {
     Comment comment = commentMapper.toDomain(commentRequest);
     CommentResponse response = commentMapper.toResponse(createCommentUseCase.add(comment));
     return new ResponseEntity<CommentResponse>(response, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping(value = "comments/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id,
+                                     @RequestHeader("Authorization") String token) {
+    deleteCommentUseCase.delete(id, token);
+    return ResponseEntity.noContent().build();
   }
 
 }
