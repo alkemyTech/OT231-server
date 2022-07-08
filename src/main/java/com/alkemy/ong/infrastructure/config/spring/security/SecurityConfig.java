614cea43c9ff97b4b4def66d08e9080d6e13c9d0
jackyos <jacky04_20@hotmail.com>
2022-07-08 20:05:58 -0300

    OT231-78: Update SecurityConfig class

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -97,6 +97,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .hasAnyRole(Role.ADMIN.name())
         .antMatchers(HttpMethod.POST, "/comments")
         .hasRole(Role.USER.name())
+        .antMatchers(HttpMethod.DELETE, "/comments/{id:[\\d+]}")
+        .hasAnyRole(Role.ADMIN.name(), Role.USER.name())
         .anyRequest()
         .authenticated()
         .and()
