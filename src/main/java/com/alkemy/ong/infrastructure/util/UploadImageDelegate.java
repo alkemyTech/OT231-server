ecb724522cce3b4136f4f0e125e648db7b48cc6f
charly5683 <ojeda.juancarlos02@gmail.com>
2022-07-11 13:43:55 -0300

    OT231-39: add exception handling in UploadImage Service

--- src/main/java/com/alkemy/ong/infrastructure/util/UploadImageDelegate.java
+++ src/main/java/com/alkemy/ong/infrastructure/util/UploadImageDelegate.java
@@ -1,12 +1,13 @@
 package com.alkemy.ong.infrastructure.util;
 
+import com.alkemy.ong.application.exception.ThirdPartyException;
 import com.alkemy.ong.application.util.IUploadImage;
+import com.alkemy.ong.domain.Image;
 import com.alkemy.ong.infrastructure.config.aws.AwsConfig;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +17,27 @@ public class UploadImageDelegate implements IUploadImage {
   @Autowired
   private AwsConfig awsConfig;
 
-  public String upload(InputStream image, String contentType, String fileName) {
+  @Override
+  public String upload(Image image) {
     AmazonS3 s3Client = awsConfig.createClientConnection();
-    s3Client.putObject(createPutObjectRequest(image, contentType, fileName));
-    return s3Client.getUrl(awsConfig.getBucketName(), fileName).toExternalForm();
+    s3Client.putObject(createPutObjectRequest(image));
+    return s3Client.getUrl(awsConfig.getBucketName(), image.getFileName()).toExternalForm();
   }
 
-  private PutObjectRequest createPutObjectRequest(InputStream input, String contentType,
-      String key) {
+  private PutObjectRequest createPutObjectRequest(Image image) {
     ObjectMetadata objectMetadata = new ObjectMetadata();
-    objectMetadata.setContentType(contentType);
+    try {
+      objectMetadata.setContentType(image.getContentType());
 
-    return new PutObjectRequest(awsConfig.getBucketName(), key, input, objectMetadata)
-        .withCannedAcl(CannedAccessControlList.PublicRead);
+      return new PutObjectRequest(
+            awsConfig.getBucketName(), 
+            image.getFileName(), 
+            image.getFile(), 
+            objectMetadata)
+          .withCannedAcl(CannedAccessControlList.PublicRead);
+    } catch (Exception e) {
+      throw new ThirdPartyException(e.getMessage());
+    }
   }
 
 }
