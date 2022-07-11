75f55af1c6a18d8e69e4bf2baa19911f5ff7c965
juuancarrizo <juuancarrizo@gmail.com>
2022-07-11 10:49:12 -0300

    OT321-84: Add documentation with Swagger

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/DocumentationResource.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.infrastructure.rest.resource;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentationResource {
+
+
+  @GetMapping("/api/docs")
+  public void swaggerRedirect(HttpServletResponse response) {
+    response.setHeader("Location", "/swagger-ui.html");
+    response.setStatus(302);
+  }
+}
