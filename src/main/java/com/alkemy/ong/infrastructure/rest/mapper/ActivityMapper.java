859c2194426770c8b0ae3b4150c6d0634d3dbfcb
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-11 14:42:28 -0300

    Add Overload toDomain Method to update in Mapper

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ActivityMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ActivityMapper.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.infrastructure.rest.mapper;
 
 import com.alkemy.ong.domain.Activity;
 import com.alkemy.ong.infrastructure.rest.request.ActivityRequest;
+import com.alkemy.ong.infrastructure.rest.request.UpdateActivityRequest;
 import com.alkemy.ong.infrastructure.rest.response.ActivityResponse;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,15 @@ public class ActivityMapper {
         .content(activityRequest.getContent())
         .image(activityRequest.getImage())
         .build();
+  }
+
+  public Activity toDomain(Long id, UpdateActivityRequest updateActivityRequest) {
+    return Activity.builder()
+            .id(id)
+            .name(updateActivityRequest.getName())
+            .content(updateActivityRequest.getContent())
+            .image(updateActivityRequest.getImage())
+            .build();
   }
 
   public ActivityResponse toResponse(Activity activity) {
