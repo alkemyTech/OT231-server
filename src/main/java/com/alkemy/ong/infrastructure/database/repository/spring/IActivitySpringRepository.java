b1bf0f49145ad2675e138ba44a4de8d5b82d9fea
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-11 14:41:20 -0300

    OT231-51 : Add findByIdAndSoftDelete methotd to Spring Repository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/spring/IActivitySpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/spring/IActivitySpringRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IActivitySpringRepository extends JpaRepository<ActivityEntity, Long> {
 
+  ActivityEntity findByIdAndSoftDeleteFalse(Long id);
 }
