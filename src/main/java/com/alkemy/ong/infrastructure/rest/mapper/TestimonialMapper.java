d4890c31238097ec37204375b3ac1c50ed057e3b
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-13 13:15:45 -0300

    OT231-65 : Add Testimonial Mapper

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/TestimonialMapper.java
@@ -0,0 +1,31 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+import com.alkemy.ong.domain.Testimonial;
+import com.alkemy.ong.infrastructure.rest.request.TestimonialRequest;
+import com.alkemy.ong.infrastructure.rest.response.TestimonialResponse;
+
+public class TestimonialMapper {
+
+  public Testimonial toDomain(TestimonialRequest testimonialRequest) {
+    if (testimonialRequest == null) {
+      return null;
+    }
+    return Testimonial.builder()
+          .name(testimonialRequest.getName())
+          .content(testimonialRequest.getContent())
+          .image(testimonialRequest.getImage())
+          .build();
+  }
+
+  public TestimonialResponse toResponse(Testimonial testimonial) {
+    if (testimonial == null) {
+      return null;
+    }
+
+    return TestimonialResponse.builder()
+          .name(testimonial.getName())
+          .content(testimonial.getContent())
+          .image(testimonial.getImage())
+          .build();
+  }
+}
