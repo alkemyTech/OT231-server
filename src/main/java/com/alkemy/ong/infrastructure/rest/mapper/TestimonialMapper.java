2497cae0eeda8bd4d90e8b09eb7d4d4bb6f6ae8e
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-13 13:41:38 -0300

    Add @Component annotation in Testimonial Mapper

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/TestimonialMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/TestimonialMapper.java
@@ -3,7 +3,9 @@ package com.alkemy.ong.infrastructure.rest.mapper;
 import com.alkemy.ong.domain.Testimonial;
 import com.alkemy.ong.infrastructure.rest.request.TestimonialRequest;
 import com.alkemy.ong.infrastructure.rest.response.TestimonialResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestimonialMapper {
 
   public Testimonial toDomain(TestimonialRequest testimonialRequest) {
