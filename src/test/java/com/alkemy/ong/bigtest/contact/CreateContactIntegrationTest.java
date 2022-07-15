e81d9213e8218f581818dd0f1a9f4b44b915e94f
evincere <elsemper@gmail.com>
2022-07-15 17:33:10 -0300

    OT231-95

--- src/test/java/com/alkemy/ong/bigtest/contact/CreateContactIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/contact/CreateContactIntegrationTest.java
@@ -1,28 +1,74 @@
 package com.alkemy.ong.bigtest.contact;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alkemy.ong.bigtest.BigTest;
-import java.nio.charset.StandardCharsets;
+import com.alkemy.ong.infrastructure.database.entity.ContactEntity;
+import com.alkemy.ong.infrastructure.rest.request.ContactRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Optional;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
 public class CreateContactIntegrationTest extends BigTest {
 
-  @Test
-  public void shouldCreateContactWhenRequestUserHasAdminRole() throws Exception {
-    saveContact(null,
-        "semper@gmail.com",
-        "message for contact I",
-        "Semper Evincere","+54448555555");
+  private static final String CREATE_CONTACT_URL = "/contacts";
+  private static final String NAME = "Semper Evincere";
+  private static final String PHONE = "+545885225885";
+  private static final String MESSAGE = "please contact me";
+  private static final String MAIL = "semper@gmail.com";
 
-    mockMvc.perform(post("/contacts")
-            .contentType(MediaType.APPLICATION_JSON)
-            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
-        .andExpect(status().isCreated())
-        .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+  @Test
+  public void shouldCreateContactWhenRequestIsValid() throws Exception {
+    /*ContactEntity contactEntity = saveContact();*/
+    mockMvc.perform(post(CREATE_CONTACT_URL)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
+            .content(createRequest(NAME, PHONE, MAIL, MESSAGE))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name", equalTo("Semper Evincere")))
+        .andExpect(jsonPath("$.phone", equalTo("+545885225885")))
+        .andExpect(jsonPath("$.email", equalTo("semper@gmail.com")))
+        .andExpect(jsonPath("$.message", equalTo("please contact me")))
+        .andExpect(status().isCreated());
+
+    assertContactHasBeenCreated(MAIL);
+  }
+
+  @Test
+  public void shouldReturn403WhenAuthTokenIsNotValid() throws Exception {
+    /*ContactEntity contactEntity = saveContact();*/
+    mockMvc.perform(post(CREATE_CONTACT_URL)
+            .header(HttpHeaders.AUTHORIZATION, "INVALID_TOKEN")
+            .content(createRequest(NAME, PHONE, MAIL, MESSAGE))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.statusCode", CoreMatchers.equalTo(403)))
+        .andExpect(jsonPath("$.message", CoreMatchers.equalTo(
+            "Access denied. Please, try to login again or contact your admin.")))
+        .andExpect(status().isForbidden());
+  }
+
+  private void assertContactHasBeenCreated(String email) {
+    Optional<ContactEntity> optionalContactEntity = Optional.of(contactRepository.findByEmail(email));
+    assertTrue(true);
+    assertThat(optionalContactEntity.get().getDeletedAt()).isNull();
+    contactRepository.delete(optionalContactEntity.get());
+  }
+
+  private String createRequest(String name, String phone, String mail, String message)
+      throws JsonProcessingException {
+    return objectMapper.writeValueAsString(ContactRequest.builder()
+        .name(name)
+        .phone(phone)
+        .email(mail)
+        .message(message)
+        .build());
   }
 
 }
