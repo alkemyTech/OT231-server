dd909a447f16b5ac0568efb7ab03b9527523e849
jackyos <jacky04_20@hotmail.com>
2022-07-16 02:29:05 -0300

    OT231-80: add List Category Domain and Pagination response

---
+++ src/main/java/com/alkemy/ong/domain/ListCategory.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListCategory {
+
+  private List<Category> categories;
+
+
+}
