e37e0b86471c7adc4403a23ed969cb3005afd291
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-17 19:47:36 -0300

    Actual Files to Solve

--- src/test/java/com/alkemy/ong/bigtest/BigTest.java
+++ src/test/java/com/alkemy/ong/bigtest/BigTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import com.alkemy.ong.OngApplication;
 import com.alkemy.ong.infrastructure.config.spring.security.common.Role;
+import com.alkemy.ong.infrastructure.database.entity.ActivityEntity;
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
 import com.alkemy.ong.infrastructure.database.entity.CommentEntity;
 import com.alkemy.ong.infrastructure.database.entity.ContactEntity;
@@ -13,6 +14,7 @@ import com.alkemy.ong.infrastructure.database.entity.OrganizationEntity;
 import com.alkemy.ong.infrastructure.database.entity.RoleEntity;
 import com.alkemy.ong.infrastructure.database.entity.SlideEntity;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import com.alkemy.ong.infrastructure.database.repository.spring.IActivitySpringRepository;
 import com.alkemy.ong.infrastructure.database.repository.spring.ICategorySpringRepository;
 import com.alkemy.ong.infrastructure.database.repository.spring.ICommentSpringRepository;
 import com.alkemy.ong.infrastructure.database.repository.spring.IContactSpringRepository;
@@ -64,6 +66,9 @@ public abstract class BigTest {
   protected ObjectMapper objectMapper;
 
   @Autowired
+  protected IActivitySpringRepository activityRepository;
+
+  @Autowired
   protected IUserSpringRepository userRepository;
 
   @Autowired
@@ -95,6 +100,7 @@ public abstract class BigTest {
     createRoles();
     createUserData();
     createNewsCategory();
+    createActivity();
   }
 
   @After
@@ -138,6 +144,12 @@ public abstract class BigTest {
   private void createNewsCategory() {
     if (categoryRepository.findByNameIgnoreCase("news") == null) {
       categoryRepository.save(buildCategory("news"));
+    }
+  }
+
+  private void createActivity(){
+    if(activityRepository.findByName("Activity") == null){
+      activityRepository.save(buildActivity("Activity", "Content", "image.png"));
     }
   }
 
@@ -198,6 +210,13 @@ public abstract class BigTest {
         .build();
   }
 
+  private ActivityEntity buildActivity(String name, String content, String image){
+    return ActivityEntity.builder()
+        .name(name)
+        .content(content)
+        .image(image)
+        .build();
+  }
   protected String getAuthorizationTokenForAdminUser() throws Exception {
     return getAuthorizationTokenForUser(ADMIN_EMAIL);
   }
@@ -244,6 +263,17 @@ public abstract class BigTest {
         .build());
 
     return organizationEntity.getId();
+  }
+
+  protected ActivityEntity saveActivity() {
+
+    return activityRepository.save(ActivityEntity.builder()
+        .name("ActyvitiS")
+        .content("Contenido")
+        .image("image.wav")
+        .softDelete(false)
+        .build());
+
   }
 
   protected SlideEntity saveSlide() {
