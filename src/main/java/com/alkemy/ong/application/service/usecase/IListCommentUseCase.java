6d2fc6aa416d79f986252368e8bdf8d1e4a6ba95
Javier Eduardo Roberti Velasquez <javiereduardorobertiv@gmail.com>
2022-07-17 19:10:42 -0300

    OT213-75 : Add List Use Case

---
+++ src/main/java/com/alkemy/ong/application/service/usecase/IListCommentUseCase.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.application.service.usecase;
+
+import com.alkemy.ong.domain.Comment;
+import java.util.List;
+
+public interface IListCommentUseCase {
+
+  List<Comment> findAll();
+
+}
