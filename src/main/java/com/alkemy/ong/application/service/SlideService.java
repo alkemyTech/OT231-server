d71006084ecbf1d3060fd81bdfee3307f64f3e86
Juan Carlos Ojeda <69003838+ojedajuanc@users.noreply.github.com>
2022-07-18 16:41:32 -0300

    OT231-57: create Slides endpoint (#88)

--- src/main/java/com/alkemy/ong/application/service/SlideService.java
+++ src/main/java/com/alkemy/ong/application/service/SlideService.java
@@ -1,18 +1,22 @@
 package com.alkemy.ong.application.service;
 
 import com.alkemy.ong.application.exception.RecordNotFoundException;
+import com.alkemy.ong.application.service.usecase.ICreateSlideUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteSlideUseCase;
 import com.alkemy.ong.application.service.usecase.IGetSlideUseCase;
 import com.alkemy.ong.application.service.usecase.IListSlideUseCase;
+import com.alkemy.ong.application.util.IUploadImage;
 import com.alkemy.ong.domain.Slide;
 import com.alkemy.ong.infrastructure.database.repository.SlideRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class SlideService implements IListSlideUseCase, IDeleteSlideUseCase, IGetSlideUseCase {
+public class SlideService
+    implements IListSlideUseCase, IDeleteSlideUseCase, IGetSlideUseCase, ICreateSlideUseCase {
 
   private final SlideRepository slideRepository;
+  private final IUploadImage uploadImage;
 
   @Override
   public List<Slide> findAllByOrderByOrder() {
@@ -34,6 +38,19 @@ public class SlideService implements IListSlideUseCase, IDeleteSlideUseCase, IGe
       throw new RecordNotFoundException("Slide not found.");
     }
     return slide;
+  }
+
+  @Override
+  public Slide create(Slide slide) {
+    slide.setImageUrl(uploadImage.upload(slide));
+    if (slide.getOrder() == null) {
+      slide.setOrder(findSubsequentOrder());
+    }
+    return slideRepository.add(slide);
+  }
+
+  private Integer findSubsequentOrder() {
+    return slideRepository.findLastKnownOrder() + 1;
   }
 
 }
