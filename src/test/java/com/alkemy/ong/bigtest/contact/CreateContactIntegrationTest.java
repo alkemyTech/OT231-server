1b6af86446bfe6a7a1db39a5f1f8aff8efd31ae6
evincere <elsemper@gmail.com>
2022-07-18 21:04:46 -0300

    FIX OT231-95

--- src/test/java/com/alkemy/ong/bigtest/contact/CreateContactIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/contact/CreateContactIntegrationTest.java
@@ -2,7 +2,8 @@ package com.alkemy.ong.bigtest.contact;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -11,17 +12,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.alkemy.ong.bigtest.BigTest;
 import com.alkemy.ong.infrastructure.database.entity.ContactEntity;
 import com.alkemy.ong.infrastructure.rest.request.ContactRequest;
+import com.alkemy.ong.infrastructure.rest.response.ContactResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.Optional;
+import javax.validation.constraints.Pattern;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 public class CreateContactIntegrationTest extends BigTest {
 
   private static final String CREATE_CONTACT_URL = "/contacts";
+
   private static final String NAME = "Semper Evincere";
-  private static final String PHONE = "+545885225885";
   private static final String MESSAGE = "please contact me";
   private static final String MAIL = "semper@gmail.com";
   private static final String INVALID_NAME = "Semper78 Evincere";
@@ -31,27 +34,38 @@ public class CreateContactIntegrationTest extends BigTest {
       + "fermentum augue consequat est.";
   private static final String ACCESS_DENIED = "Access denied. Please, try to login again or "
       + "contact your admin.";
+  private ContactResponse contactResponse;
+  private String string;
 
   @Test
   public void shouldCreateContactWhenRequestIsValid() throws Exception {
     mockMvc.perform(post(CREATE_CONTACT_URL)
             .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
-            .content(createRequest())
+            .content(createRequest(NAME, MAIL, MESSAGE))
             .contentType(APPLICATION_JSON))
-        .andExpect(jsonPath("$.name", equalTo(NAME)))
-        .andExpect(jsonPath("$.phone", equalTo(PHONE)))
+        .andExpectAll().andExpect(jsonPath("$.name", equalTo(NAME)))
         .andExpect(jsonPath("$.email", equalTo(MAIL)))
         .andExpect(jsonPath("$.message", equalTo(MESSAGE)))
-        .andExpect(status().isCreated());
+        .andExpect(status().isCreated())
+        .andDo(MvcResult -> {
+          String json = MvcResult.getResponse().getContentAsString();
+          contactResponse = objectMapper.readValue(json, ContactResponse.class);
+    });
 
-    assertContactHasBeenCreated();
+    assertContactHasBeenCreated(contactResponse.getEmail());
+  }
+
+  private void assertContactHasBeenCreated(String contactEmail) {
+    ContactEntity contactEntity = contactRepository.findByEmail(contactEmail);
+    assertNotNull(contactEntity);
+    assertThat(contactEntity.getDeletedAt()).isNull();
   }
 
   @Test
   public void shouldReturn403WhenAuthTokenIsNotValid() throws Exception {
     mockMvc.perform(post(CREATE_CONTACT_URL)
             .header(HttpHeaders.AUTHORIZATION, "INVALID_TOKEN")
-            .content(createRequest())
+            .content(createRequest(NAME,MAIL,MESSAGE))
             .contentType(APPLICATION_JSON))
         .andExpect(jsonPath("$.statusCode", CoreMatchers.equalTo(403)))
         .andExpect(jsonPath("$.message", CoreMatchers.equalTo(
@@ -60,87 +74,47 @@ public class CreateContactIntegrationTest extends BigTest {
   }
 
   @Test
-  public void fieldsRequestShouldValidate() throws Exception {
+  public void shouldReturnStatusCode400WhenNameIsNotValid() throws Exception {
     mockMvc.perform(post(CREATE_CONTACT_URL)
-            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
-            .contentType(APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(createRequest())))
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(createRequest(INVALID_NAME, MAIL,MESSAGE)))
+        .andExpect(jsonPath("$.moreInfo",
+            hasItems("Name can only contain letters and whitespaces")))
+        .andExpect(jsonPath("$.statusCode", equalTo(400)))
         .andExpect(status().isBadRequest());
   }
 
   @Test
-  public void fieldNameRequestNotValidate() throws Exception {
-      mockMvc.perform(post(CREATE_CONTACT_URL)
-      .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
-      .contentType(APPLICATION_JSON)
-      .content(objectMapper.writeValueAsString(createBadRequestFieldName())))
-          .andExpect(status().isBadRequest());
-  }
-
-  @Test
-  public void fieldMailRequestNotValidate() throws Exception {
+  public void shouldReturnStatusCode400WhenEmailIsNotValid() throws Exception {
     mockMvc.perform(post(CREATE_CONTACT_URL)
-            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
-            .contentType(APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(createBadRequestFieldMail())))
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(createRequest(NAME, INVALID_MAIL,MESSAGE)))
+        .andExpect(jsonPath("$.moreInfo",
+            hasItems("Email should be valid")))
+        .andExpect(jsonPath("$.statusCode", equalTo(400)))
         .andExpect(status().isBadRequest());
   }
 
   @Test
-  public void fieldMessageRequestNotValidate() throws Exception {
+  public void shouldReturnStatusCode400WhenMessageIsNotValid() throws Exception {
     mockMvc.perform(post(CREATE_CONTACT_URL)
-            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser())
-            .contentType(APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(createBadRequestFieldMessageMaxCharacters())))
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(createRequest(NAME, MAIL, INVALID_MESSAGE)))
+        .andExpect(jsonPath("$.moreInfo",
+            hasItems("Message must have a maximum of 150 characters")))
+        .andExpect(jsonPath("$.statusCode", equalTo(400)))
         .andExpect(status().isBadRequest());
   }
 
-  private String createBadRequestFieldMessageMaxCharacters()
+  private String createRequest(String name, String email, String message)
       throws JsonProcessingException {
     return objectMapper.writeValueAsString(ContactRequest.builder()
-        .name(CreateContactIntegrationTest.NAME)
-        .phone(CreateContactIntegrationTest.MAIL)
-        .message(CreateContactIntegrationTest.INVALID_MESSAGE)
-        .build());
-  }
-
-  private String createBadRequestFieldName()
-      throws JsonProcessingException {
-    return objectMapper.writeValueAsString(ContactRequest.builder()
-        .name(CreateContactIntegrationTest.INVALID_NAME)
-        .email(CreateContactIntegrationTest.MAIL)
-        .message(CreateContactIntegrationTest.MESSAGE)
-        .build());
-  }
-
-  private void assertContactHasBeenCreated() {
-    Optional<ContactEntity> optionalContactEntity = Optional.of(contactRepository.findByEmail(
-        CreateContactIntegrationTest.MAIL));
-    assertTrue(true);
-    assertThat(optionalContactEntity.get().getDeletedAt()).isNull();
-    assertThat(optionalContactEntity.get().getName()).isEqualTo(NAME);
-    assertThat(optionalContactEntity.get().getEmail()).isEqualTo(MAIL);
-    assertThat(optionalContactEntity.get().getMessage()).isEqualTo(MESSAGE);
-    assertThat(optionalContactEntity.get().getPhone()).isEqualTo(PHONE);
-    contactRepository.delete(optionalContactEntity.get());
-  }
-
-  private String createRequest()
-      throws JsonProcessingException {
-    return objectMapper.writeValueAsString(ContactRequest.builder()
-        .name(CreateContactIntegrationTest.NAME)
-        .phone(CreateContactIntegrationTest.PHONE)
-        .email(CreateContactIntegrationTest.MAIL)
-        .message(CreateContactIntegrationTest.MESSAGE)
-        .build());
-  }
-
-  private String createBadRequestFieldMail()
-      throws JsonProcessingException {
-    return objectMapper.writeValueAsString(ContactRequest.builder()
-        .name(CreateContactIntegrationTest.NAME)
-        .phone(CreateContactIntegrationTest.INVALID_MAIL)
-        .message(CreateContactIntegrationTest.MESSAGE)
+        .name(name)
+        .email(email)
+        .message(message)
         .build());
   }
 
