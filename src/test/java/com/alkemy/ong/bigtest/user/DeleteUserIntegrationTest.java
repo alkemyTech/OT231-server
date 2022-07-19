25e8757c025d75604a09c67bdec9bdc0f7699e9b
juuancarrizo <juuancarrizo@gmail.com>
2022-07-19 00:27:19 -0300

    OT231-90: Test User

---
+++ src/test/java/com/alkemy/ong/bigtest/user/DeleteUserIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.alkemy.ong.bigtest.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.alkemy.ong.bigtest.BigTest;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import java.util.Optional;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+public class DeleteUserIntegrationTest extends BigTest {
+
+  private static final String DELETE_USER_URL = "/users/{id}";
+
+  @Test
+  public void shouldDeleteUserWhenRequestHasAdminRole() throws Exception {
+    Long randomUserId = getRandomUser().getId();
+    mockMvc.perform(delete(DELETE_USER_URL, String.valueOf(randomUserId))
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
+            .andExpect(status().isNoContent());
+
+    assertUserHasBeenDeleted(randomUserId);
+  }
+
+  @Test
+  public void shouldReturn403WhenMissingAuthToken() throws Exception {
+    mockMvc.perform(delete(DELETE_USER_URL, "1")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.statusCode", equalTo(403)))
+            .andExpect(jsonPath("$.message",
+                    equalTo("Access denied. Please, try to login again or contact your admin.")))
+            .andExpect(status().isForbidden());
+  }
+
+  @Test
+  public void shouldReturn404WhenUserDoesNotExist() throws Exception {
+    mockMvc.perform(delete(DELETE_USER_URL, "100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
+                .andExpect(jsonPath("$.statusCode", equalTo(404)))
+                .andExpect(jsonPath("$.message", equalTo("Record not found in database.")))
+                .andExpect(jsonPath("$.moreInfo", hasSize(1)))
+                .andExpect(jsonPath("$.moreInfo", hasItem("User not found.")))
+                .andExpect(status().isNotFound());
+  }
+
+  private void assertUserHasBeenDeleted(Long id) {
+    Optional<UserEntity> optionalUserEntity = userRepository.findById(id);
+    assertTrue(optionalUserEntity.isPresent());
+    assertThat(optionalUserEntity.get().getSoftDelete()).isTrue();
+  }
+
+}
---
+++ src/test/java/com/alkemy/ong/bigtest/user/GetUserIntegrationTest.java
@@ -0,0 +1,71 @@
+package com.alkemy.ong.bigtest.user;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.alkemy.ong.bigtest.BigTest;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import com.alkemy.ong.infrastructure.database.repository.spring.IUserSpringRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+
+public class GetUserIntegrationTest extends BigTest {
+
+  @Autowired
+  private IUserSpringRepository userSpringRepository;
+
+  @Test
+  public void shouldReturnUsersListWhenAdminIsAuthenticated() throws Exception{
+    mockMvc.perform(get("/users")
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser())
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.users", hasSize(3)))
+        .andExpect(jsonPath("$.users[0].email", equalTo("jason@voorhees.com")))
+        .andExpect(jsonPath("$.users[1].email", equalTo("freedy@krueger.com")))
+        .andExpect(jsonPath("$.users[2].email", equalTo("john@connors.com")))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void shouldNotReturnUserDeletedInUsersList() throws Exception{
+    createdUserWhitSoftDeleteTrue();
+    mockMvc.perform(get("/users")
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser())
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.users", hasSize(3)))
+            .andExpect(jsonPath("$.users[0].email", equalTo("jason@voorhees.com")))
+            .andExpect(jsonPath("$.users[1].email", equalTo("freedy@krueger.com")))
+            .andExpect(jsonPath("$.users[2].email", equalTo("john@connors.com")))
+            .andExpect(status().isOk());
+  }
+
+  private void createdUserWhitSoftDeleteTrue() {
+    UserEntity user = UserEntity.builder()
+            .firstName("pedro")
+            .lastName("paez")
+            .email("mdsd@gmail.com")
+            .password("abcd1234")
+            .softDelete(true)
+            .build();
+    userSpringRepository.save(user);
+
+  }
+
+  @Test
+  public void shouldReturn403WhenAuthTokenIsNotAdmin() throws Exception {
+    mockMvc.perform(get("/users")
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
+        .andExpect(jsonPath("$.statusCode", CoreMatchers.equalTo(403)))
+        .andExpect(jsonPath("$.message", equalTo(
+          "Access denied. Please, try to login again or contact your admin.")))
+        .andExpect(status().isForbidden());
+  }
+
+}
