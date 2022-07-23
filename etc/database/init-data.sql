c97727ee1f4b10094d1a1186c2c48a5589b2caea
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-07-23 17:42:09 -0300

    update init-data.sql

--- etc/database/init-data.sql
+++ etc/database/init-data.sql
@@ -1,19 +1,19 @@
-insert into organizations (organization_id, about_us_text, address, create_timestamp, email,
+insert into public.organizations (organization_id, about_us_text, address, create_timestamp, email,
                                   facebook_url, image, instagram_url, linkedin_url, name,
                                   phone_number, soft_delete, welcome_text)
 values (1, 'We are Somos Mas', '742 Evergreen Terrace', null, 'somosmas@org-somosmas.com',
 		'www.facebook.com/Somos_Mas', 'https://alkemy-java-ot231.s3.amazonaws.com/somosmas.png',
-		'www.instagram.com/SomosMas', null, 'Somos Mas', '+5491160112988', false,
-		'All set! Now you have full access to our web page');
+		'www.instagram.com/SomosMas', null, 'Somos Mas', '5491160112988', false,
+		'All set! Now you have full access to our web page!');
 
-insert into categories (category_id, create_timestamp, description, image, name, soft_delete)
+insert into public.categories (category_id, create_timestamp, description, image, name, soft_delete)
 values (1, null, 'news', 'https://app.s3.com/news.jpg', 'news', false);
 
-insert into news (news_id, content, create_timestamp, image, name, soft_delete, category_id)
+insert into public.news (news_id, content, create_timestamp, image, name, soft_delete, category_id)
 values (1, 'This is the news content...', null,
         'https://alkemy-java-ot231.s3.amazonaws.com/workshop.jpg', 'Last Week Workshop', false, 1);
 
-insert into members (member_id, name, facebook_url, instagram_url, linkedin_url, image,
+insert into public.members (member_id, name, facebook_url, instagram_url, linkedin_url, image,
                             description, create_timestamp, soft_delete)
 values
 (1, 'Maria Iraola', null, null, null,
