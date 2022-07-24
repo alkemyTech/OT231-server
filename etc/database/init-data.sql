5beeca5ba24bae15370b628f8cf5e9391cfb4769
Juan Carlos Ojeda <69003838+ojedajuanc@users.noreply.github.com>
2022-07-24 05:04:46 -0300

    Update init-data.sql & general fixes (#94)

--- etc/database/init-data.sql
+++ etc/database/init-data.sql
@@ -1,10 +1,35 @@
 insert into public.organizations (organization_id, about_us_text, address, create_timestamp, email,
                                   facebook_url, image, instagram_url, linkedin_url, name,
                                   phone_number, soft_delete, welcome_text)
-values (1, 'We are Somos Mas', '742 Evergreen Terrace', null, 'somosmas@org-somosmas.com', null,
-        'https://app.s3.com/somosmas.jpg', null, null, 'Somos Mas', '+549115789456', false,
-        'Welcome to Somos Mas');
+values (1, 'We are Somos Mas', '742 Evergreen Terrace', null, 'somosmas@org-somosmas.com',
+		'www.facebook.com/Somos_Mas', 'https://alkemy-java-ot231.s3.amazonaws.com/somosmas.png',
+		'www.instagram.com/SomosMas', null, 'Somos Mas', '5491160112988', false,
+		'All set! Now you have full access to our web page!');
 
 insert into public.categories (category_id, create_timestamp, description, image, name, soft_delete)
 values (1, null, 'news', 'https://app.s3.com/news.jpg', 'news', false);
+
+insert into public.news (news_id, content, create_timestamp, image, name, soft_delete, category_id)
+values (1, 'This is the news content...', null,
+        'https://alkemy-java-ot231.s3.amazonaws.com/workshop.jpg', 'Last Week Workshop', false, 1);
+
+insert into public.members (member_id, name, facebook_url, instagram_url, linkedin_url, image,
+                            description, create_timestamp, soft_delete)
+values
+(1, 'Maria Iraola', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/miraola.jpg','Presidenta', null, false),
+(2, 'Marita Gomez', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/mgomez.jpeg', 'Fundadora', null, false),
+(3, 'Miriam Rodriguez', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/mrodriguez.jpg', 'Terapista ocupacional', null, false),
+(4, 'Cecilia Mendez', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/cmendez.jpeg', 'Psicopedagoga', null, false),
+(5, 'Mario Fuentes', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/#', 'Psicologo', null, false),
+(6, 'Rodrigo Fuente', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/rfuente.jpg', 'Contador', null, false),
+(7, 'Maria Garcia', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/mgarcia.jpg', 'Profesora de Artes Dramaticas', null, false),
+(8, 'Marco Fernandez', null, null, null,
+'https://alkemy-java-ot231.s3.amazonaws.com/mfernandez.jpg', 'Profesor de Educacion Fisica', null, false);
 
--- src/main/java/com/alkemy/ong/infrastructure/rest/response/SocialMediaResponse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/SocialMediaResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.infrastructure.rest.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SocialMediaResponse {
 
   private String facebookUrl;
